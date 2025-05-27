76f7653acd668d94cd716baa8c5e06b7c5c0efdb
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-05-27 17:04:18 -0300

    :tada: Commit inicial

---
+++ src/main/java/br/com/fiap/climby/ClimbyApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.climby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/climby/climby/ClimbyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.climby.climby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
