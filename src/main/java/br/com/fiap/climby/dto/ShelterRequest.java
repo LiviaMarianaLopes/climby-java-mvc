165336e0465600b873894fad1d51e1ff2e6bf972
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-05-28 19:56:16 -0300

    :sparkles: feat: dto

---
+++ src/main/java/br/com/fiap/climby/dto/ShelterRequest.java
@@ -0,0 +1,40 @@
+package br.com.fiap.climby.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class ShelterRequest {
+
+    @NotBlank(message = "{name.not.blank}")
+    @Size(min = 3, max = 100, message = "{name.size}")
+    private String name;
+
+    @NotBlank(message = "{email.not.blank}")
+    @Email(message = "{email.valid}")
+    private String email;
+
+    @NotNull(message = "{phone.not.null}")
+    private Long phone;
+
+    @NotBlank(message = "{country.not.blank}")
+    private String country;
+
+    @NotBlank(message = "{city.not.blank}")
+    private String city;
+
+    @NotBlank(message = "{address.not.blank}")
+    private String adress;
+
+    @NotNull(message = "{addressNumber.not.null}")
+    private int adressNumber;
+
+    @NotBlank(message = "{cep.not.blank}")
+    @Pattern(regexp = "\\d{8}", message = "{cep.valid}")
+    private int cep;
+
+    @NotBlank(message = "{district.not.blank}")
+    private String district;
+
+    private boolean isFull;
+}
---
+++ src/main/java/br/com/fiap/climby/dto/UserRequest.java
@@ -0,0 +1,24 @@
+package br.com.fiap.climby.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+public class UserRequest {
+    @NotBlank(message = "{name.not.blank}")
+    @Size(min = 3, max = 100, message = "{name.size}")
+    private String name;
+
+    @Email(message = "{email.valid}")
+    @NotBlank(message = "{email.not.blank}")
+    private String email;
+
+    @NotBlank(message = "{country.not.blank}")
+    private String country;
+
+    @NotBlank(message = "{city.not.blank}")
+    private String city;
+
+}
