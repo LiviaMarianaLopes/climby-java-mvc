af76b0f93dcfe37acb58253067e208bf07a5eac5
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-05-28 19:55:40 -0300

    :sparkles: feat: entidades

---
+++ src/main/java/br/com/fiap/climby/model/Shelter.java
@@ -0,0 +1,25 @@
+package br.com.fiap.climby.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "CY_SHELTER")
+@Data
+public class Shelter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private Long phone;
+    private String country;
+    private String city;
+    private String adress;
+    @Column(name = "ADRESS_NUMBER")
+    private int adressNumber;
+    private int cep;
+    private String district;
+    @Column(name = "IS_FULL")
+    private boolean isFull;
+}
---
+++ src/main/java/br/com/fiap/climby/model/User.java
@@ -0,0 +1,19 @@
+package br.com.fiap.climby.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "CY_USER")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "FIREBASE_UID")
+    private String firebaseUID;
+    private String name;
+    private String email;
+    private String country;
+    private String city;
+}
